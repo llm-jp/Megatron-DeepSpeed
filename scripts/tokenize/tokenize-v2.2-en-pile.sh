e6e9ff2760012c0aeac3ba16bf53125c71001ecd
kazuki <kaz.tokyo.tech20@gmail.com>
2023-10-09 14:39:45 +0900

    feat: tokenize script

---
+++ scripts/tokenize/tokenize-v2.2-en-pile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#$ -l rt_AF=1
+#$ -l h_rt=10:00:00
+#$ -j y
+#$ -o outputs/tokenize/
+#$ -cwd
+
+set -e
+
+# module load
+source /etc/profile.d/modules.sh
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
+
+# python virtualenv
+cd /bb/llm/gaf51275/llm-jp/Megatron-DeepSpeed
+source .env/bin/activate
+
+# set the input directory
+INPUT_DIR=/bb/llm/gaf51275/llm-jp/datasets/llm-jp-corpus/108b
+
+# set the output directory
+OUTPUT_DIR=/bb/llm/gaf51275/llm-jp/datasets/binarized/v1.0.2/code20K_en40K_ja60K.ver2.2
+mkdir -p $OUTPUT_DIR
+
+# set the model directory
+MODEL_DIR=/bb/llm/gaf51275/llm-jp/llm-ja-tokenizer/models/ver2
+MODEL_PATH=$MODEL_DIR/code20K_en40K_ja60K.ver2.2.model
+
+# Tokenize and binarize
+
+# refined en-pile & en-pile
+for file in $INPUT_DIR/*pile*; do
+  if [ -f "$file" ]; then
+    python tools/preprocess_data.py \
+      --input "$file" \
+      --output-prefix "$OUTPUT_DIR/$(basename "$file" .${file##*.})" \
+      --tokenizer-model "$MODEL_PATH" \
+      --dataset-impl mmap \
+      --tokenizer-type SentencePieceTokenizer \
+      --workers 64 \
+      --append-eod
+  fi
+done
