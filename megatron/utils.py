6dd155daa8d72bb4c57bd0ae25cbd42b2b97fdc4
kazuki <kaz.tokyo.tech20@gmail.com>
2023-12-19 20:03:26 +0900

    feat: tflops calculation equations

--- megatron/utils.py
+++ megatron/utils.py
@@ -288,15 +288,15 @@ def get_model_parameters(model) -> tuple[int, int]:
 
 
 def throughput_calculator(model, args, iteration_time, total_iterations):
-    batch_size = args.micro_batch_size * get_num_microbatches() * args.data_parallel_size
-    approx_parameters_in_billions = None if (model is None) else get_parameters_in_billions(model)
-    elapsed_time_per_iter = iteration_time/total_iterations
+    batch_size: int = args.micro_batch_size * get_num_microbatches() * args.data_parallel_size
+    elapsed_time_per_iter = iteration_time / total_iterations
     samples_per_second = batch_size / elapsed_time_per_iter
 
-    #flops calculator
-    hidden_size = args.hidden_size
-    num_layers = args.num_layers
-    vocab_size = args.padded_vocab_size
+    # flops calculator
+    hidden_size: int = args.hidden_size
+    num_layers: int = args.num_layers
+    vocab_size: int = args.padded_vocab_size
+    intermediate_size: int = args.ffn_hidden_size
 
     # General TFLOPs formula (borrowed from Equation 3 in Section 5.1 of
     # https://arxiv.org/pdf/2104.04473.pdf).
@@ -305,14 +305,27 @@ def throughput_calculator(model, args, iteration_time, total_iterations):
     checkpoint_activations_factor = 3
     if hasattr(args, 'checkpoint_activations') and args.checkpoint_activations:
         checkpoint_activations_factor = 4
-    if hasattr(args, 'recompute_granularity') and (args.recompute_granularity == 'selective' or args.recompute_granularity == 'full'):
+    if hasattr(args, 'recompute_granularity') and args.recompute_granularity == 'full':
         checkpoint_activations_factor = 4
-    seq_len = args.seq_length
+    if hasattr(args, 'recompute_granularity') and args.recompute_granularity == 'selective':
+        checkpoint_activations_factor = 3.5
+
+    seq_len: int = args.seq_length
     if hasattr(args, 'actual_seq_length'):
         seq_len = args.actual_seq_length
-    flops_per_iteration = (24 * checkpoint_activations_factor * batch_size * seq_len * num_layers * (hidden_size**2)) * (1. + (seq_len / (6. * hidden_size)) + (vocab_size / (16. * num_layers * hidden_size)))
-    tflops = flops_per_iteration / (elapsed_time_per_iter * args.world_size * (10**12))
+
+    activation_function_factor: int = 4  # GELU
+    if args.swiglu:
+        activation_function_factor = 4 + 2  # SWiGLU (upscaling + down scaling)
+
+    flops_per_iteration: float = (
+        (8 + activation_function_factor * (intermediate_size / hidden_size)) * checkpoint_activations_factor * batch_size * seq_len * num_layers * (hidden_size**2)
+    ) * (1. + (seq_len / (6. * hidden_size)) + (vocab_size / (16. * num_layers * hidden_size)))
+
+    tflops: float = flops_per_iteration / (elapsed_time_per_iter * args.world_size * (10**12))
+
     return samples_per_second, tflops
+
 
 def checkpoint_throughput_calculator(model, latency_second):
     approx_parameters_in_billions = get_parameters_in_billions(model)
