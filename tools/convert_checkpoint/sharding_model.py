1d210fb22544721fa5b92dd43ec6ffbfa2cc5b9f
Hiroshi Matsuda <40782025+hiroshi-matsuda-rit@users.noreply.github.com>
2023-12-31 19:54:33 +0900

    trust_remote_code

--- tools/convert_checkpoint/sharding_model.py
+++ tools/convert_checkpoint/sharding_model.py
@@ -7,7 +7,7 @@ from transformers import AutoModelForCausalLM
 def main():
   src = sys.argv[1]
   dst = sys.argv[2]
-  model = AutoModelForCausalLM.from_pretrained(src, torch_dtype=torch.bfloat16)
+  model = AutoModelForCausalLM.from_pretrained(src, torch_dtype=torch.bfloat16, trust_remote_code=True)
   model.save_pretrained(dst)
 
 
